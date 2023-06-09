503a1089c3c279c4bf6f830028d13f36fc828d56
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-06-09 14:59:35 +0500

    Working cache example

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  async getHello(): Promise<string> {
+    const cachedData: string = await this.cacheManager.get('key');
+    if (cachedData) {
+      return cachedData;
+    }
+
+    await this.cacheManager.set('key', 'Hello World from Cache!');
     return 'Hello World!';
   }
 }
